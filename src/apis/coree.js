5f9723474d34b3e5e532d7c0b3cc1ee7c2b7a849
sgm4247 <sgm4247@naver.com>
2023-12-19 05:01:47 +0900

    feat >> data 저장

--- src/apis/coree.js
+++ src/apis/coree.js
@@ -4,13 +4,13 @@ import { useEffect } from "react";
 const API_KEY = `29b1ddf8120a76b9c30ea01353da61f0`;
 // const CITY = `Seoul`;
 const LANG = `kr`;
-//lat 위도 lon경도
+//lat 위도 lon경도 &units=metric = 섭씨로 변경
 export const axiosInstance = axios.create({
-  baseURL: `https://api.openweathermap.org/data/2.5/weather?lat=37.54&lon=126&lang=${LANG}&appid=${API_KEY}`,
+  baseURL: `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&lang=${LANG}`,
 });
-
-const getdata = async () => {
-  const res = await axiosInstance.get();
+//&lat=37.5683&lon=126.9778&units=metric
+const getdata = async ({ lat, lon }) => {
+  const res = await axiosInstance.get(`&lat=${lat}&lon=${lon}8&units=metric`);
   return console.log(res.data);
 };
 
@@ -20,7 +20,10 @@ export const Api = {
 
 const Page2 = () => {
   useEffect(() => {
-    getdata();
+    getdata({
+      lat: 37.5683,
+      lon: 126.9778,
+    });
   }, []);
   return <>didi</>;
 };
@@ -28,13 +31,18 @@ export default Page2;
 
 // 오 일단 설정값으로 부르니 데이터는 불러와진다
 /* 
+
+목표 : https://jennnn.tistory.com/44
 [12/17]
 오늘 한일
 아이피 키 만들기 
 axios연결
 데이터 가져온거 console 확인 
+
 내일 할일
 데이터 정리 인자로 받을 수 있도록 정리 
+서울만했어
+
 화면에 어떻게 띄울지 정리 
 피그마든 구상해서 디자인 완료 완료하기!!
 
