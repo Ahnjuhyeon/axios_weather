bf24333686095e28b7d7df5aeb52ddc59c2d2bca
sgm4247 <sgm4247@naver.com>
2023-12-21 04:17:04 +0900

    feat >> 파일생성

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import Page from "./apis/core";
 import Page2 from "./apis/coree";
+import MainPage from "./pages";
 
 function App() {
   // return <Page />;
-  return <Page2 />;
+  // return <Page2 />;
+  return <MainPage></MainPage>;
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+const MainPage = () => {
+  return (
+    <>
+      <p>city time</p>
+      <div>
+        <button>LA</button>
+        <button>LONDON</button>
+        <button>PARIS</button>
+        <button>SEOUL</button>
+        <button>SYDNEY</button>
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
